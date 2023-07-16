1401231962f7f6c74e156204d3be767771495d28
Fanis <f.shigapov@inbox.ru>
2023-07-16 21:52:36 +0600

    Домашнее задание 19

---
+++ Hw3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+int CountDigits(int inputNumber)
+{   
+    int countDigit = 0;
+
+    for (; inputNumber > 0; countDigit++)
+        inputNumber /=10;
+
+    return countDigit;
+}
+bool Palindrom(int inputNumber, int countNUmbers)
+{
+    for (; countNUmbers > 0;)
+    {
+        int degreeOfTen = (int)(Math.Pow(10, countNUmbers - 1));
+        int FirstDigit = inputNumber / degreeOfTen;
+        int lastDigit = inputNumber % 10;
+        if (FirstDigit != lastDigit)
+            return false;
+
+        inputNumber -= FirstDigit * degreeOfTen;
+        inputNumber /=10;
+        countNUmbers -= 2;
+    }
+    
+    return true;
+}
+
+Console.Clear();
+System.Console.WriteLine("Программа выдает палиндром");
+System.Console.WriteLine("Введите число: ");
+int inputNumber = int.Parse(Console.ReadLine());
+int digits = CountDigits(inputNumber);
+System.Console.WriteLine(Palindrom(inputNumber, digits));
+
