efebfaaff3b3ac6d622c1b9a2214a7f40645bd8b
Fanis <f.shigapov@inbox.ru>
2023-07-16 21:55:11 +0600

    Домашнее задание 21

--- Hw3/Program.cs
+++ Hw3/Program.cs
@@ -1,36 +1,58 @@
 ﻿// Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
-int CountDigits(int inputNumber)
-{   
-    int countDigit = 0;
+// int CountDigits(int inputNumber)
+// {   
+//     int countDigit = 0;
 
-    for (; inputNumber > 0; countDigit++)
-        inputNumber /=10;
+//     for (; inputNumber > 0; countDigit++)
+//         inputNumber /=10;
 
-    return countDigit;
-}
-bool Palindrom(int inputNumber, int countNUmbers)
-{
-    for (; countNUmbers > 0;)
-    {
-        int degreeOfTen = (int)(Math.Pow(10, countNUmbers - 1));
-        int FirstDigit = inputNumber / degreeOfTen;
-        int lastDigit = inputNumber % 10;
-        if (FirstDigit != lastDigit)
-            return false;
+//     return countDigit;
+// }
+// bool Palindrom(int inputNumber, int countNUmbers)
+// {
+//     for (; countNUmbers > 0;)
+//     {
+//         int degreeOfTen = (int)(Math.Pow(10, countNUmbers - 1));
+//         int FirstDigit = inputNumber / degreeOfTen;
+//         int lastDigit = inputNumber % 10;
+//         if (FirstDigit != lastDigit)
+//             return false;
 
-        inputNumber -= FirstDigit * degreeOfTen;
-        inputNumber /=10;
-        countNUmbers -= 2;
-    }
+//         inputNumber -= FirstDigit * degreeOfTen;
+//         inputNumber /=10;
+//         countNUmbers -= 2;
+//     }
     
-    return true;
+//     return true;
+// }
+
+// Console.Clear();
+// System.Console.WriteLine("Программа выдает палиндром");
+// System.Console.WriteLine("Введите число: ");
+// int inputNumber = int.Parse(Console.ReadLine());
+// int digits = CountDigits(inputNumber);
+// System.Console.WriteLine(Palindrom(inputNumber, digits));
+
+//Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+Console.Clear();
+(double x, double y, double z) GetPointFromConsole(string message)
+{
+    System.Console.WriteLine(message);
+    double x = GetNumberFromConsole("x: ");
+    double y = GetNumberFromConsole("y: ");
+    double z = GetNumberFromConsole("z:");
+    
+    return (x, y, z);
+}
+int GetNumberFromConsole (string message)
+{
+    System.Console.WriteLine(message);
+    return Int32.Parse(Console.ReadLine());
 }
 
-Console.Clear();
-System.Console.WriteLine("Программа выдает палиндром");
-System.Console.WriteLine("Введите число: ");
-int inputNumber = int.Parse(Console.ReadLine());
-int digits = CountDigits(inputNumber);
-System.Console.WriteLine(Palindrom(inputNumber, digits));
-
+(double x, double y, double z) point1 = GetPointFromConsole("Введите точку А: ");
+(double x, double y, double z) point2 = GetPointFromConsole("Введите точку B: ");
+double answer = Math.Round(Math.Sqrt(Math.Pow(point2.x - point1.x, 2) +
+    Math.Pow(point2.y - point1.y, 2) + Math.Pow(point2.z - point1.z, 2)), 2);
+System.Console.WriteLine("Расстояние между двумя точками в 3Д пространстве: " + answer);
